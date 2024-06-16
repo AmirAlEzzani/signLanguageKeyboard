58d8c32da50121f8a7fc63d72f5aa2a84de8bacf
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-06-15 23:07:00 -0700

    function to detect webcam, displays output when ran

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/imgcodecs.hpp>
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include<opencv2/objdetect.hpp>
+#include<iostream>
 
+using namespace std;
 using namespace cv;
-int main() {
-    Mat image;
-    VideoCapture cam(0);
-    namedWindow("camera");
 
-    while (1) {
-        cam >> img;
-        imshow("camera", img);
-
-        if (waitKey(1) == 27) // esc key
-            break;
-    }
-    cam.release();
-    return 0;
+void main() {
+	VideoCapture video(1);
+	Mat img;
+	while (true) {
+		video.read(img);
+		imshow("Frame", img);
+		waitKey(1);
+	}
 }
