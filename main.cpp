b2b3117ac948d73f9d0cc4a57dd527873311a895
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-06-16 23:06:57 -0700

    face detection test

--- main.cpp
+++ main.cpp
@@ -9,10 +9,20 @@ using namespace cv;
 
 void main() {
 	VideoCapture video(1);
+	CascadeClassifier facedetect;
 	Mat img;
+	facedetect.load("haarcascade+frontalface_default.xml");
+
 	while (true) {
-		video.read(img);
+		vector<Rect> faces;
+		facedetect.detectMultiScale(img, faces, 1.3, 5);
+
+		for (int i = 0; i < faces.size(); i++) {
+			rectangle(img, faces[i].tl(), faces[i].br(), Scalar(50, 50, 255), 3);
+		}
+
 		imshow("Frame", img);
 		waitKey(1);
 	}
 }
+
